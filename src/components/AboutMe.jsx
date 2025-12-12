f0d7a229e18df64a20cf6a0b0c7068ec73760dc0
kavyan256 <iit2024245@iiita.ac.in>
2025-12-12 21:23:44 +0530

    removed duplicate text from aboutme below marquee

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -20,19 +20,6 @@ export default function About() {
       </div>
 
       <MarqueeGSAP />
-    
-      {/* Bottom text */}
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="text-center space-y-14 max-w-7xl">
-          <p className="text-4xl leading-relaxed text-gray-800">
-            I’m Kavyan, a developer specializing in low-level systems, and exploring AI/ML, and creative web experiences.  
-            I enjoy understanding and building how things work beneath the surface and turning ideas into code.
-          </p>
-          <p className="text-3xl font-light leading-relaxed text-gray-600">
-            I specialize in low-level systems and performance-driven engineering, and I’m expanding into full-stack development and AI/ML to build smarter, interactive digital experiences.
-          </p>
-        </div>
-      </div>
 
     </section>
   );
