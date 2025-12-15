9319b12cb0f75358adbda05a1ed51cc39eeb1727
kavyan256 <iit2024245@iiita.ac.in>
2025-12-15 12:33:58 +0530

    rebased changes

--- src/utils/analyticsApi.js
+++ src/utils/analyticsApi.js
@@ -1,5 +1,4 @@
 import { io } from "socket.io-client";
-import cors from "cors";
 
 const API_URL = "https://immediate-newfoundland-peripheral-latex.trycloudflare.com/";
 const API_TIMEOUT = 5000; // 5 second timeout
